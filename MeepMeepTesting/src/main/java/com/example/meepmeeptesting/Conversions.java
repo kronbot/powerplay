8f94adfd259fb5cbd375223b52b08657b16b59e6
AlexDavid26 <alexbleotu2006@gmail.com>
2022-10-18 15:26:54 +0300

    Added conversions

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Conversions.java
@@ -0,0 +1,11 @@
+package com.example.meepmeeptesting;
+
+public class Conversions {
+    public static double inchToCm(double valueInInch) {
+        return 2.54 * valueInInch;
+    }
+
+    public static double cmToInch(double valueInCm) {
+        return valueInCm / 2.54;
+    }
+}

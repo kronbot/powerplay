0426ba434763d2c2d3eb7e313d68eb01334a1d4c
cristiioan <67168425+cristiioan@users.noreply.github.com>
2022-10-18 10:01:23 +0300

    Cod pt mers la cel mai inalt stalp

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -11,17 +12,13 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints( 52.48180821614297,  52.48180821614297, Math.toRadians(184.02607784577722), Math.toRadians(184.02607784577722), 16.34)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-39, -61, Math.toRadians(90)))
+                                // cod pentru mers la cel mai apropiat stalp inalt
+                                .splineToSplineHeading(new Pose2d(-60, -24, Math.toRadians(90)), Math.toRadians(90))
+                                //.splineToSplineHeading(new Pose2d(-58, -25, Math.toRadians(90)), Math.toRadians(90))
+                                .splineToSplineHeading(new Pose2d(-32, -6, Math.toRadians(27)), Math.toRadians(0))
                                 .build()
                 );
 

7185192bf70722a9723b0bd54131111093f29ca9
AlexDavid26 <alexbleotu2006@gmail.com>
2022-10-19 15:01:31 +0300

    Added Conversions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Utils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Utils.java
@@ -23,4 +23,12 @@ public class Utils {
     ) {
         return (x - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin;
     }
+
+    public static double inchToCm(double valueInInch) {
+        return 2.54 * valueInInch;
+    }
+
+    public static double cmToInch(double valueInCm) {
+        return valueInCm / 2.54;
+    }
 }

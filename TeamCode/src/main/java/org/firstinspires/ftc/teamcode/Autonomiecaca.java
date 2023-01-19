b7b990c7d25c9cce053c3852efc6a7c72da6b4fc
Istratie Stefan <109197365+IstratieStefan@users.noreply.github.com>
2023-01-19 15:25:44 +0200

    da

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomiecaca.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomiecaca.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -8,21 +10,37 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 @Config
 @Autonomous(name = "random")
 public class Autonomiecaca extends LinearOpMode{
     @Override
     public void runOpMode(){
-        SampleMecanumDrive drivetrain=new SampleMecanumDrive(hardwareMap);
-        waitForStart();
-        Trajectory  FirstCone= drivetrain.trajectoryBuilder(new Pose2d(0,0,0))
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        SampleMecanumDrive drive =new SampleMecanumDrive(hardwareMap);
+        Trajectory FirstCone = drive.trajectoryBuilder(new Pose2d(0,0,0))
                 .lineTo(new Vector2d(40,5))
-                        .build();
-        drivetrain.followTrajectory(FirstCone);
-        Trajectory  FirstCone2= drivetrain.trajectoryBuilder(new Pose2d(40,5,Math.toRadians(90)))
+                .build();
+        Trajectory FirstCone2 = drive.trajectoryBuilder(new Pose2d(40,5,Math.toRadians(90)))
                 .lineToSplineHeading(new Pose2d(60, 10, Math.toRadians(50)))
                 .build();
-        drivetrain.followTrajectory(FirstCone2);
+        waitForStart();
+        drive.followTrajectory(FirstCone);
+        drive.followTrajectory(FirstCone2);
+
+        Pose2d poseEstimate = drive.getPoseEstimate();
+
+        while(opModeIsActive())
+        {
+            telemetry.addData("finalX", poseEstimate.getX());
+            telemetry.addData("finalY", poseEstimate.getY());
+            telemetry.addData("finalHeading", poseEstimate.getHeading());
+            telemetry.update();
+        }
+
+
+        while (!isStopRequested() && opModeIsActive()) {}
     }
 }

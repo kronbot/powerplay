4bf1fc141585d8dea85ea1a47f8eff0e80790904
AlexDavid26 <82370626+AlexDavid26@users.noreply.github.com>
2023-02-04 17:23:52 +0200

    Code working :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KronBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KronBot.java
@@ -22,6 +22,10 @@ public class KronBot {
     public Encoder rightEncoder;
     public Encoder frontEncoder;
 
+    public DcMotor leftEncoderMotor;
+    public DcMotor rightEncoderMotor;
+    public DcMotor frontEncoderMotor;
+
     /**
      * Initialization of hardware map
      */
@@ -50,6 +54,15 @@ public class KronBot {
         rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "rightEncoder"));
         frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontEncoder"));
         leftEncoder.setDirection(Encoder.Direction.REVERSE);
+
+        leftEncoderMotor = hardwareMap.dcMotor.get("leftEncoder");
+        rightEncoderMotor = hardwareMap.dcMotor.get("rightEncoder");
+        frontEncoderMotor = hardwareMap.dcMotor.get("frontEncoder");
+
+
+        leftEncoderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightEncoderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontEncoderMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     /**

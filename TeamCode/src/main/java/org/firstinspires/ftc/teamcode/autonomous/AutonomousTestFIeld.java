a120bc764a747b8889d3e31200174b7cf5dba259
AlexDavid26 <alexbleotu2006@gmail.com>
2023-02-17 10:48:11 +0200

    field inspection autonomie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousTestFIeld.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.KronBot;
+
+@Autonomous
+public class AutonomousTestFIeld extends LinearOpMode {
+    private final KronBot robot = new KronBot();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.initHardwareMap(hardwareMap);
+
+        waitForStart();
+
+        robot.drive(1, 1, 1, 1, 1);
+        sleep(500);
+        robot.stopMotors();
+
+        while (opModeIsActive()) {};
+    }
+}

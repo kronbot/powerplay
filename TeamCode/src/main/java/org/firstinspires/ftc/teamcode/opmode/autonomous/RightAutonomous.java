265b156d4c413f3017c2c50f4ab49519d37001ac
AlexDavid26 <82370626+AlexDavid26@users.noreply.github.com>
2023-03-03 20:55:27 +0200

    sefu peste tot astia imi zic el comandante

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/RightAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/RightAutonomous.java
@@ -29,14 +29,14 @@ public class RightAutonomous extends LinearOpMode {
 
     public static double throwConeX = 58, throwConeY = 5;
     public static double prepareInitialX = 45, prepareInitialY = 5;
-    public static double initialJunctionX = 57.5, initialJunctionY = 13, initialJunctionHeading = 30;
+    public static double initialJunctionX = 57.5, initialJunctionY = 12.5, initialJunctionHeading = 30;
 
     public static double getConeX = 52.5, getConeY = -19, getConeHeading = -90;
 
     public static double junctionX = 53.5, junctionY = 18.5, junctionHeading = 90;
     public static double junctionStraightX = 55.5, junctionStraightY = 17.25;
 
-    public static double parkingX = 51, parkingYPos1 = 28, parkingYPos2 = 4, parkingYPos3 = -18, parkingHeading = -90;
+    public static double parkingX = 51, parkingYPos1 = 27, parkingYPos2 = 8, parkingYPos3 = -18, parkingHeading = -90;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -64,7 +64,7 @@ public class RightAutonomous extends LinearOpMode {
         TrajectorySequence goToCones = drive
                 .trajectorySequenceBuilder(new Pose2d(initialJunctionX, initialJunctionY, Math.toRadians(initialJunctionHeading)))
                 .setReversed(true)
-                .lineTo(new Vector2d(initialJunctionX - 2, initialJunctionY + 2))
+                .lineTo(new Vector2d(initialJunctionX - 5, initialJunctionY - 5))
                 .lineToSplineHeading(new Pose2d(getConeX, getConeY, Math.toRadians(getConeHeading)))
                 .build();
         TrajectorySequence goToJunction = drive
